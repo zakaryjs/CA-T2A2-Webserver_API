fcb439452f3f7479eeec1f65d87e811960f27abc
zakaryjs <119669322+zakaryjs@users.noreply.github.com>
2023-07-12 13:22:30 +0800

    Added foreign key relationships and back_populates to collection.py model

--- models/collection.py
+++ models/collection.py
@@ -4,8 +4,12 @@ class Collection(db.Model):
     __tablename__ = 'collections'
 
     id = db.Column(db.Integer, primary_key=True)
-    movie_id =
-    book_id = 
+
+    movie_id = db.Column(db.Integer, db.ForeignKey('movies.id'))
+    book_id = db.Column(db.Integer, db.ForeignKey('books.id'))
+
+    movie = db.relationship('Movie', back_populates=('collections'))
+    book = db.relationship('Book', back_populates=('collections'))
 
 class CollectionSchema(ma.Schema):
     class Meta:
